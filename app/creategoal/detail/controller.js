8ddf0c81133ff8000093c5e1eff13db86268bf62
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-10-19 15:08:03 -0400

    now able to add individual goals, but the relationship is not clearly defined

--- app/creategoal/detail/controller.js
+++ app/creategoal/detail/controller.js
@@ -2,20 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     formValues: {
-        priority: '',
+        // priority: '',
         content: '',
     },
 
     actions: {
         save() {
-            if (this.formValues.priority == 'bigThing') {
+            // if (this.formValues.priority == 'bigThing') {
                 const goal = this.store.createRecord('bigThing', {
                     name: this.formValues.content,
                     finished: false,
                 });
                 goal.save();
                 this.set('formvalues', {});
-            }
+            // }
         }
     }
 });
