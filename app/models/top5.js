1e4ffe2bc24ddc1070e6f47427a9c29bb6184022
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-11-30 15:24:16 -0500

    sync up with kyle

--- app/models/top5.js
+++ app/models/top5.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  goals: DS.hasMany('goal', { async: true }),
-  personals: DS.hasMany('personal', { async: true }),
-  user: DS.attr('string'),
+  goals: DS.hasMany('goal'),
+  // personals: DS.hasMany('personal', { async: true }),
+  // user: DS.attr('string'),
   timeframe: DS.attr('string'),
 });
